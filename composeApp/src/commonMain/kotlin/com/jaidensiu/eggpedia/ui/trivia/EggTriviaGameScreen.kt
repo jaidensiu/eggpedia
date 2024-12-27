e63f770e1f6f8df38208991dc02be59e0f8d0ae2
jaidensiu <jaidensiu@gmail.com>
2024-12-27 00:34:48 -0800

    chore: adding files for egg trivia game

---
+++ composeApp/src/commonMain/kotlin/com/jaidensiu/eggpedia/ui/trivia/EggTriviaGameScreen.kt
@@ -0,0 +1,5 @@
+package com.jaidensiu.eggpedia.ui.trivia
+
+class EggTriviaGameScreen {
+    // TODO: can only play with internet
+}
---
+++ composeApp/src/commonMain/kotlin/com/jaidensiu/eggpedia/ui/trivia/EggTriviaGameScreenState.kt
@@ -0,0 +1,5 @@
+package com.jaidensiu.eggpedia.ui.trivia
+
+data class EggTriviaGameScreenState(
+    val isConnectedToInternet: Boolean = false
+)
