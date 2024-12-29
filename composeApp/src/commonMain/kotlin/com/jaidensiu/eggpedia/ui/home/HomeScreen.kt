794cc0238770196f97fc736478ce566454fb3f89
jaidensiu <jaidensiu@gmail.com>
2024-12-28 22:04:14 -0800

    feat: adding egg icon

--- composeApp/src/commonMain/kotlin/com/jaidensiu/eggpedia/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jaidensiu/eggpedia/ui/home/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.jaidensiu.eggpedia.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
@@ -16,6 +19,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import eggpedia.composeapp.generated.resources.Res
+import eggpedia.composeapp.generated.resources.egg_icon
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -35,6 +41,11 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(Res.drawable.egg_icon),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(fraction = 0.5f)
+        )
         Text(
             text = "Good ${state.value.timeOfDay}",
             color = MaterialTheme.colors.onBackground
